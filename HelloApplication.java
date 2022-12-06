2b7b9b2fcc9500601a8577caecc1a2a1452fc888
Joran <48070495+Joranvnp@users.noreply.github.com>
2022-12-06 17:29:10 +0100

    Modification Hash

---
+++ HelloApplication.java
@@ -0,0 +1,26 @@
+package com.example.projetjavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("accueil.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ HelloController.java
@@ -0,0 +1,75 @@
+package com.example.projetjavafx;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.sql.*;
+
+public class HelloController {
+    @FXML
+    private TextField monID;
+    @FXML
+    private PasswordField monMdp;
+
+    @FXML
+    protected void onHelloButtonClick() throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
+        String URL = "jdbc:mysql://172.19.0.9:3306/ProjetCle";
+
+        String LOGIN = "phpmyadmin";
+        String PASSWORD = "0550002D";
+        try {
+
+            Connection connexion = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+
+            Statement stmt = connexion.createStatement();
+            String req = "SELECT login, password, droit FROM users";
+            ResultSet res1 = stmt.executeQuery(req);
+            res1.next();
+
+            int droit = res1.getInt("droit");
+            String login = res1.getString("login");
+            String mdp = res1.getString("test");
+
+            Hasher hasher = Hashing.sha256().newHasher();
+            hasher.putString("admin", Charsets.UTF_8);
+            HashCode sha256 = hasher.hash();
+
+            System.out.println(mdp);
+
+            if (login.equals(login) && monMdp.getText().equals(mdp) && mdp.equals(mdp)) {
+                Stage newWindow = new Stage();
+                FXMLLoader fxmlLoader = new
+                        FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+                Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+                newWindow.setScene(scene);
+                // Specifies the modality for new window.
+                newWindow.initModality(Modality.APPLICATION_MODAL);
+                newWindow.show();
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Erreur de connexion");
+                alert.setHeaderText("Connexion");
+                alert.setContentText("identifiant erroné");
+                alert.showAndWait();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

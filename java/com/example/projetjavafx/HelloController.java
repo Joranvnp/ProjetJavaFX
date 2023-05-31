0e16b147c801c1597593e33321f662bcec214caa
Ythikone <116084267+Ythikone@users.noreply.github.com>
2023-05-31 11:31:21 +0200

    Add files via upload

--- java/com/example/projetjavafx/HelloController.java
+++ java/com/example/projetjavafx/HelloController.java
@@ -1,35 +1,138 @@
-package com.example.projetjavafx;
+package com.example.tpjavafx;
 
 import com.google.common.base.Charsets;
 import com.google.common.hash.HashCode;
 import com.google.common.hash.Hasher;
 import com.google.common.hash.Hashing;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-
 import java.sql.*;
-public class HelloController {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HelloController extends HelloApplication{
     @FXML
     private TextField monID;
     @FXML
     private PasswordField monMdp;
 
     @FXML
-    protected void onHelloButtonClick() throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
-        String URL = "jdbc:mysql://172.19.0.9:3306/ProjetCle";
+    private TextField numCle;
 
-        String LOGIN = "phpmyadmin";
-        String PASSWORD = "0550002D";
+    @FXML
+    private TextField coulCle;
+
+    @FXML
+    private TextField ouvertureCle;
+
+    public String URL = "jdbc:mysql://172.19.0.27:3306/tpcle";
+
+    public String LOGIN = "phpmyadmin";
+
+    public String PASSWORD = "0550002D";
+
+    @FXML
+    private ComboBox<Cle> liste ;
+
+    protected ObservableList<Cle> listCle;
+
+    public List<Cle> laliste = new ArrayList<>();
+
+     public  static void main(String[] args){
+//
+         launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) throws IOException, SQLException {
+        liste = new ComboBox<>();
+        liste.setPromptText("Sélectionnez une clé");
+        liste.setItems(listCle);
+        listCle = FXCollections.observableArrayList(getTteCle());
+        try {
+            Label colorLabel = new Label("Couleur :");
+            coulCle = new TextField();
+            Label descriptionLabel = new Label("Description :");
+            ouvertureCle = new TextField();
+            Label numberLabel = new Label("Numéro :");
+            numCle = new TextField();
+            Button addButton = new Button("Ajouter");
+            Button updateButton = new Button("Modifier");
+            Button deleteButton = new Button("Supprimer");
+
+
+            liste.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                if (newValue != null) {
+                    coulCle.setText(newValue.getCouleur());
+                    ouvertureCle.setText(newValue.getOuverture());
+                    numCle.setText(String.valueOf(newValue.getNumero()));
+                }
+            });
+
+//            addButton.setOnAction(e -> addKey());
+//            updateButton.setOnAction(e -> updateKey());
+//            deleteButton.setOnAction(e -> deleteKey());
+
+            GridPane gridPane = new GridPane();
+            gridPane.setHgap(10);
+            gridPane.setVgap(10);
+            gridPane.add(liste, 0, 0, 3, 1);
+            gridPane.addRow(1, colorLabel, coulCle);
+            gridPane.addRow(2, descriptionLabel, ouvertureCle);
+            gridPane.addRow(3, numberLabel, numCle);
+            gridPane.addRow(4, addButton, updateButton, deleteButton);
+
+            Scene scene = new Scene(gridPane, 400, 200);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Gestion des clés");
+            listCle.addAll(getTteCle());
+            primaryStage.show();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private List<Cle> getTteCle() throws SQLException {
+
+
+            Connection connexion = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            try {
+                Statement stmt = connexion.createStatement();
+                String req = "SELECT * FROM cle";
+                ResultSet res1 = stmt.executeQuery(req);
+                while (res1.next()) {
+                    int numCle = res1.getInt("numero");
+
+                    String couleurCle = res1.getString("couleur");
+                    String ouvertureCle = res1.getString("ouverture");
+                    Cle cle1 = new Cle(numCle, couleurCle, ouvertureCle);
+                    laliste.add(cle1);
+
+
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return laliste;
+        }
+
+    @FXML
+    protected void onHelloButtonClick() throws NoSuchAlgorithmException, NoSuchProviderException, IOException {
+
+
+
+
         try {
 
             Connection connexion = DriverManager.getConnection(URL, LOGIN, PASSWORD);
@@ -71,9 +174,36 @@ public class HelloController {
                 alert.showAndWait();
             }
 
-    }
+        }
         catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public void onSearchButtonClick(ActionEvent actionEvent) {
+
     }
+
+    public void onModifButtonClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    protected void onAddButtonClick()throws NoSuchAlgorithmException, NoSuchProviderException, IOException  {
+        try {
+            Cle cle = new Cle(Integer.parseInt(numCle.getText()),coulCle.getText(),ouvertureCle.getText());
+            Connection connexion = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+
+            Statement stmt = connexion.createStatement();
+            String req = "INSERT INTO cle (numero, couleur, ouverture) VALUES (" + cle.getNumero() + ", '" + cle.getCouleur() + "', '" + cle.getOuverture() + "')";
+            stmt.executeUpdate(req);
+
+
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void onSuppButtonClick(ActionEvent actionEvent) {
+    }
+}
